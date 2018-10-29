68148f07e86d1f885b51261a76a31613bfc7f89e
Azadeh Khojandi <azkhojan@microsoft.com>
2018-10-29 14:32:44 +1100

    update arch param

--- maskrcnn/clone.sh
+++ maskrcnn/clone.sh
@@ -8,6 +8,10 @@ else
     echo "going inside repo"
     cd pytorch-mask-rcnn
     ls
+    chmod 777 /home/$NB_USER/work/pytorch-mask-rcnn
+    echo "getting weights"
+    wget 'https://azpublicblob.blob.core.windows.net/public/mask_rcnn_coco.pth'
+    wget 'https://azpublicblob.blob.core.windows.net/public/resnet50_imagenet.pth'
     echo "build 1"
     cd nms/src/cuda/
     nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=$GPU_Arch
@@ -16,7 +20,7 @@ else
     cd ../
     echo "build 2"
     cd roialign/roi_align/src/cuda/
-    nvcc -c -o crop_and_resize_kernel.cu.o crop_and_resize_kernel.cu -x cu -Xcompiler -fPIC -arch=GPU_Arch
+    nvcc -c -o crop_and_resize_kernel.cu.o crop_and_resize_kernel.cu -x cu -Xcompiler -fPIC -arch=$GPU_Arch
     cd ../../
     python build.py
     cd ../../
@@ -26,7 +30,5 @@ else
     cd cocoapi/PythonAPI
     make
     cd ../..
-    echo "getting weights"
-    wget 'https://azpublicblob.blob.core.windows.net/public/mask_rcnn_coco.pth'
-    wget 'https://azpublicblob.blob.core.windows.net/public/resnet50_imagenet.pth'
+    
 fi
