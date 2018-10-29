b83b35024aa8ffb21c3059638fa9e19f58133089
Azadeh Khojandi <azkhojan@microsoft.com>
2018-10-29 13:47:27 +1100

    update clone.sh

--- maskrcnn/clone.sh
+++ maskrcnn/clone.sh
@@ -1,43 +1,33 @@
-
 echo /home/$NB_USER/work/pytorch-mask-rcnn
-
+chmod 777 /home/$NB_USER/work/
 if [ -d /home/$NB_USER/work/pytorch-mask-rcnn ]   
 then 
     echo "repo exist"
 else
     echo "repo doesn't exist"
-    
     git clone https://github.com/multimodallearning/pytorch-mask-rcnn.git
-    
     echo "going inside repo"
     cd pytorch-mask-rcnn
     ls
-    
     echo "build 1"
     cd nms/src/cuda/
     nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=$GPU_Arch
     cd ../../
     python build.py
     cd ../
-
     echo "build 2"
     cd roialign/roi_align/src/cuda/
     nvcc -c -o crop_and_resize_kernel.cu.o crop_and_resize_kernel.cu -x cu -Xcompiler -fPIC -arch=GPU_Arch
     cd ../../
     python build.py
     cd ../../
-
     echo "add cocoapi as submodule"
     git submodule add https://github.com/cocodataset/cocoapi.gi pytorch-mask-rcnn/cocoapi
-
     echo "build/settings cocoapi"
     cd cocoapi/PythonAPI
     make
     cd ../..
-
     echo "getting weights"
     wget 'https://azpublicblob.blob.core.windows.net/public/mask_rcnn_coco.pth'
     wget 'https://azpublicblob.blob.core.windows.net/public/resnet50_imagenet.pth'
-
-
 fi
